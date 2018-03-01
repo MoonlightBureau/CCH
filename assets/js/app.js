6299b41b5385a79fc84c3989e89d1943bc498dab
Dan Rader <howdy@danrdesign.com>
2018-03-01 13:57:32 -0500

    Animations first stab

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,16 +16,59 @@ $(document).ready(function() {
 //
 // instance.start()
 
-const instance = basicScroll.create({
+const rotate = basicScroll.create({
 	elem: document.querySelector('.cch-open'),
 	from: 'top-bottom',
-	to: 'top-top',
+	to: 'bottom-top',
 	props: {
-		'--rotate': {
+		'--r': {
 			from: '-45deg',
 			to: '45deg'
 		}
 	}
 })
 
-instance.start()
+const fadeBox = basicScroll.create({
+        elem: document.querySelector('.hero-second'),
+        from: 'middle-middle',
+        to: 'middle-middle',
+        inside: (instance, percentage, props) => console.log('fadeBox is animating'),
+        outside: (instance, percentage, props) => console.log('fadeBox is not animating'),
+        props: {
+            '--o': {
+                from: .01,
+                to: .99
+            }
+        }
+    })
+
+    const fadeBox_rev = basicScroll.create({
+            elem: document.querySelector('.hero-second'),
+            from: 'middle-middle',
+            to: 'middle-middle',
+            inside: (instance, percentage, props) => console.log('fadeBox is animating'),
+            outside: (instance, percentage, props) => console.log('fadeBox is not animating'),
+            props: {
+                '--or': {
+                    from: .99,
+                    to: .01
+                }
+            }
+        })
+//
+// const colorShift = basicScroll.create({
+// 	elem: document.querySelector('.flipflop-blue'),
+//     from: 'top-bottom',
+// 	to: 'bottom-top',
+// 	props: {
+// 		'--c': {
+// 			from: 'blue',
+// 			to: 'green'
+// 		}
+// 	}
+// })
+
+rotate.start()
+fadeBox.start()
+fadeBox_rev.start()
+// colorShift.start()
