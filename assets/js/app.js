68039544e2eaec8bb6e0010d02ab7cbc016c9a6e
Dan Rader <howdy@danrdesign.com>
2018-03-01 12:28:05 -0500

    Fucking squiggles 🐙

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,13 +2,27 @@ $(document).ready(function() {
     $(".cch-open").lettering();
 });
 
+// const instance = basicScroll.create({
+// 	elem: document.querySelector('.cch-open span:nth-child(odd)'),
+// 	from: 'top-bottom',
+// 	to: 'middle-middle',
+// 	props: {
+// 		'--rotate': {
+// 			from: '0',
+// 			to: '45deg'
+// 		}
+// 	}
+// })
+//
+// instance.start()
+
 const instance = basicScroll.create({
-	elem: document.querySelector('.cch-open span:nth-child(odd)'),
+	elem: document.querySelector('.cch-open'),
 	from: 'top-bottom',
-	to: 'middle-middle',
+	to: 'top-top',
 	props: {
 		'--rotate': {
-			from: '0',
+			from: '-45deg',
 			to: '45deg'
 		}
 	}
