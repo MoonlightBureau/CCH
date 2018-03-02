44cc68630b6da1c19ef92cfc33cd70dca8199e33
Daniel Rader <howdy@danrdesign.com>
2018-03-02 09:56:27 -0500

    Updated Hero Bg and 3 other files

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,19 +2,6 @@ $(document).ready(function() {
     $(".cch-open").lettering();
 });
 
-// const instance = basicScroll.create({
-// 	elem: document.querySelector('.cch-open span:nth-child(odd)'),
-// 	from: 'top-bottom',
-// 	to: 'middle-middle',
-// 	props: {
-// 		'--rotate': {
-// 			from: '0',
-// 			to: '45deg'
-// 		}
-// 	}
-// })
-//
-// instance.start()
 
 const rotate = basicScroll.create({
 	elem: document.querySelector('.cch-open'),
@@ -28,47 +15,17 @@ const rotate = basicScroll.create({
 	}
 })
 
-const fadeBox = basicScroll.create({
-        elem: document.querySelector('.hero-second'),
-        from: 'middle-middle',
-        to: 'middle-middle',
-        inside: (instance, percentage, props) => console.log('fadeBox is animating'),
-        outside: (instance, percentage, props) => console.log('fadeBox is not animating'),
-        props: {
-            '--o': {
-                from: .01,
-                to: .99
-            }
-        }
-    })
-
-    const fadeBox_rev = basicScroll.create({
-            elem: document.querySelector('.hero-second'),
-            from: 'middle-middle',
-            to: 'middle-middle',
-            inside: (instance, percentage, props) => console.log('fadeBox is animating'),
-            outside: (instance, percentage, props) => console.log('fadeBox is not animating'),
-            props: {
-                '--or': {
-                    from: .99,
-                    to: .01
-                }
-            }
-        })
-//
-// const colorShift = basicScroll.create({
-// 	elem: document.querySelector('.flipflop-blue'),
-//     from: 'top-bottom',
-// 	to: 'bottom-top',
-// 	props: {
-// 		'--c': {
-// 			from: 'blue',
-// 			to: 'green'
-// 		}
-// 	}
-// })
+const moveUp = basicScroll.create({
+	elem: document.querySelector('.move-up'),
+	from: 'top-top',
+	to: 'bottom-top',
+	props: {
+		'--yu': {
+			from: '0',
+			to: '20rem'
+		}
+	}
+})
 
 rotate.start()
-fadeBox.start()
-fadeBox_rev.start()
-// colorShift.start()
+moveUp.start()
