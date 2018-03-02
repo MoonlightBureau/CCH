1a69e49259122bd7e794ee5e4e8689cfc3c1b2fc
Dan Rader <howdy@danrdesign.com>
2018-03-02 09:28:53 -0500

    Kill color shift

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,19 +2,6 @@ $(document).ready(function() {
     $(".cch-open").lettering();
 });
 
-// const instance = basicScroll.create({
-// 	elem: document.querySelector('.cch-open span:nth-child(odd)'),
-// 	from: 'top-bottom',
-// 	to: 'middle-middle',
-// 	props: {
-// 		'--rotate': {
-// 			from: '0',
-// 			to: '45deg'
-// 		}
-// 	}
-// })
-//
-// instance.start()
 
 const rotate = basicScroll.create({
 	elem: document.querySelector('.cch-open'),
@@ -28,47 +15,34 @@ const rotate = basicScroll.create({
 	}
 })
 
-const fadeBox = basicScroll.create({
-        elem: document.querySelector('.hero-second'),
-        from: 'middle-middle',
-        to: 'middle-middle',
-        inside: (instance, percentage, props) => console.log('fadeBox is animating'),
-        outside: (instance, percentage, props) => console.log('fadeBox is not animating'),
-        props: {
-            '--o': {
-                from: .01,
-                to: .99
-            }
-        }
-    })
-
-    const fadeBox_rev = basicScroll.create({
-            elem: document.querySelector('.hero-second'),
-            from: 'middle-middle',
-            to: 'middle-middle',
-            inside: (instance, percentage, props) => console.log('fadeBox is animating'),
-            outside: (instance, percentage, props) => console.log('fadeBox is not animating'),
-            props: {
-                '--or': {
-                    from: .99,
-                    to: .01
-                }
-            }
-        })
+// const fadeBox = basicScroll.create({
+//         elem: document.querySelector('.hero-second'),
+//         from: 'middle-middle',
+//         to: 'middle-middle',
+//         inside: (instance, percentage, props) => console.log('fadeBox is animating'),
+//         outside: (instance, percentage, props) => console.log('fadeBox is not animating'),
+//         props: {
+//             '--o': {
+//                 from: .01,
+//                 to: .99
+//             }
+//         }
+//     })
 //
-// const colorShift = basicScroll.create({
-// 	elem: document.querySelector('.flipflop-blue'),
-//     from: 'top-bottom',
-// 	to: 'bottom-top',
-// 	props: {
-// 		'--c': {
-// 			from: 'blue',
-// 			to: 'green'
-// 		}
-// 	}
-// })
+//     const fadeBox_rev = basicScroll.create({
+//             elem: document.querySelector('.hero-second'),
+//             from: 'middle-middle',
+//             to: 'middle-middle',
+//             inside: (instance, percentage, props) => console.log('fadeBox is animating'),
+//             outside: (instance, percentage, props) => console.log('fadeBox is not animating'),
+//             props: {
+//                 '--or': {
+//                     from: .99,
+//                     to: .01
+//                 }
+//             }
+//         })
 
 rotate.start()
-fadeBox.start()
-fadeBox_rev.start()
-// colorShift.start()
+// fadeBox.start()
+// fadeBox_rev.start()
