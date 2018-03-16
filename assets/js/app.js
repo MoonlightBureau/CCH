2151862db1feb683e35131fffafd86431a386a11
Daniel Rader <howdy@danrdesign.com>
2018-03-15 23:30:50 -0400

    Updated Location and 4 other files

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,11 +4,9 @@ $( document ).ready(function(){
     $('.cch-open').find('span').filter( ':even' ).addClass('letters-even');
 
     document.querySelectorAll('.move-vert').forEach((elem) => {
-
     	const modifier = elem.getAttribute('data-modifier')
         const from = elem.getAttribute('data-from')
         const to = elem.getAttribute('data-to')
-
     	basicScroll.create({
     		elem: elem,
             from: from,
@@ -23,50 +21,51 @@ $( document ).ready(function(){
     	}).start()
     });
 
-const rotate = basicScroll.create({
-	elem: document.querySelector('.letters-odd'),
-	from: 'middle-bottom',
-	to: 'bottom-top',
-	props: {
-		'--r': {
-			from: '0',
-			to: '45deg'
-		}
-	}
-})
+    document.querySelectorAll('.letters-odd').forEach((elem) => {
+    	basicScroll.create({
+    		elem: elem,
+            from: 'middle-bottom',
+        	to: 'bottom-top',
+        	props: {
+        		'--r': {
+        			from: '0',
+        			to: '45deg'
+        		}
+        	}
+    	}).start()
+    });
 
-const rotateRev = basicScroll.create({
-	elem: document.querySelector('.letters-even'),
-	from: 'middle-bottom',
-	to: 'bottom-top',
-	props: {
-		'--rr': {
-			from: '0',
-			to: '-45deg'
-		}
-	}
-});
+    document.querySelectorAll('.letters-even').forEach((elem) => {
+    	basicScroll.create({
+    		elem: elem,
+            from: 'middle-bottom',
+        	to: 'bottom-top',
+        	props: {
+        		'--rr': {
+        			from: '0',
+        			to: '-45deg'
+        		}
+        	}
+    	}).start()
+    });
 
-document.querySelectorAll('.moneyPiece').forEach((elem) => {
+    document.querySelectorAll('.moneyPiece').forEach((elem) => {
 
-	const modifier = elem.getAttribute('data-modifier')
+    	const modifier = elem.getAttribute('data-modifier')
 
-	basicScroll.create({
-		elem: elem,
-        from: 'middle-middle',
-    	to: 'bottom-top',
-		direct: true,
-		props: {
-			'--translateY': {
-				from: '0',
-				to: `${ 2.4 * modifier }px`
-			}
-		}
-	}).start()
+    	basicScroll.create({
+    		elem: elem,
+            from: 'middle-middle',
+        	to: 'bottom-top',
+    		direct: true,
+    		props: {
+    			'--translateY': {
+    				from: '0',
+    				to: `${ 2.4 * modifier }px`
+    			}
+    		}
+    	}).start()
 
-});
+    });
 
-
-    rotate.start()
-    rotateRev.start()
 });
