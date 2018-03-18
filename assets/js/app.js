c6fc7c9c85ec5895b11e9fac036a3306381525d0
Daniel Rader <howdy@danrdesign.com>
2018-03-18 14:27:47 -0400

    Updated Petworth Holiday Carolling and 75 other files

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,7 @@ $( document ).ready(function(){
     $('.cch-open').find('span').filter( ':odd' ).addClass('letters-odd');
     $('.cch-open').find('span').filter( ':even' ).addClass('letters-even');
 
+
     document.querySelectorAll('.move-vert').forEach((elem) => {
     	const modifier = elem.getAttribute('data-modifier')
         const from = elem.getAttribute('data-from')
@@ -65,7 +66,41 @@ $( document ).ready(function(){
     			}
     		}
     	}).start()
-
     });
+
+//————————————> DEBUG ME
+
+    $('.cch-events').lettering();
+    $('.cch-events').find('span').filter( ':odd' ).addClass('events-odd');
+    $('.cch-events').find('span').filter( ':even' ).addClass('events-even');
+    document.querySelectorAll('.events-odd').forEach((elem) => {
+    	basicScroll.create({
+    		elem: elem,
+            from: 'middle-bottom',
+        	to: 'bottom-top',
+        	props: {
+        		'--events-rotate': {
+        			from: '0',
+        			to: '45deg'
+        		}
+        	}
+    	}).start()
+    });
+
+    document.querySelectorAll('.events-even').forEach((elem) => {
+    	basicScroll.create({
+    		elem: elem,
+            from: 'middle-bottom',
+        	to: 'bottom-top',
+        	props: {
+        		'--events-rotate-r': {
+        			from: '0',
+        			to: '-45deg'
+        		}
+        	}
+    	}).start()
+    });
+
+// DEBUG ME <————————————
 
 });
