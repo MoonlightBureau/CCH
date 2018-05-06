56f614bcc6bd06277c48feff14fa61c171c99ac4
Dan Rader <howdy@danrdesign.com>
2018-05-06 09:14:42 -0400

    Abandoned the Ajax mailchim for now

--- assets/js/app.js
+++ assets/js/app.js
@@ -142,27 +142,27 @@ $( document ).ready(function(){
         });
     });
 
-// MAILCHIMP FUNCTION
-
-$('.myform').submit(function(e) {
-  var $this = $(this);
-  $.ajax({
-      type: "GET", // GET & url for json slightly different
-      url: "http://XXXXXXXX.list-manage2.com/subscribe/post-json?c=?",
-      data: $this.serialize(),
-      dataType    : 'json',
-      contentType: "application/json; charset=utf-8",
-      error       : function(err) { alert("Could not connect to the registration server."); },
-      success     : function(data) {
-          if (data.result != "success") {
-              // Something went wrong, parse data.msg string and display message
-          } else {
-              // It worked, so hide form and display thank-you message.
-          }
-      }
-  });
-  return false;
-});
+// MAILCHIMP FUNCTION--leaving this for now
+//
+// $('.myform').submit(function(e) {
+//   var $this = $(this);
+//   $.ajax({
+//       type: "GET", // GET & url for json slightly different
+//       url: "http://capitolciderhouse.list-manage2.com/subscribe/post-json?c=?",
+//       data: $this.serialize(),
+//       dataType    : 'json',
+//       contentType: "application/json; charset=utf-8",
+//       error       : function(err) { alert("Could not connect to the registration server."); },
+//       success     : function(data) {
+//           if (data.result != "success") {
+//               // Something went wrong, parse data.msg string and display message
+//           } else {
+//               // It worked, so hide form and display thank-you message.
+//           }
+//       }
+//   });
+//   return false;
+// });
 
 // END DOCUMENT READY
 });
